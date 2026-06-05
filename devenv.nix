b29bf15a56464422cb2cc30fb76486493cefa682
celeri <celestin.bouchet@gmail.com>
2026-06-05 13:22:01 +0200

    add devenv conf

---
+++ devenv.nix
@@ -0,0 +1,9 @@
+{
+  # Node is used to add locally test and depencies to functions
+  languages.javascript = {
+    enable = true;
+    lsp.enable = false;
+    nodejs.enable = true;
+    npm.enable = true;
+  };
+}
